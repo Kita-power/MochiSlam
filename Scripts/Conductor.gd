a816bb2d4ad33ab62bbd2da343f747f15fd0c895
Kita-power <ec24380@qmul.ac.uk>
2026-03-12 13:22:45 +0000

    Initial Godot rhythm game project

---
+++ Scripts/Conductor.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var bpm = 120.0
+var seconds_per_beat
+var song_position = 0.0
+var song_position_in_beats = 0.0
+
+var music_player: AudioStreamPlayer
+
+func _ready():
+	seconds_per_beat = 60.0 / bpm
+
+func _process(delta):
+	if music_player:
+		song_position = music_player.get_playback_position()
+		song_position_in_beats = song_position / seconds_per_beat
