5b4ee7b7e7ef64cb0a8cca9dcfdce26321a07a8b
Kita-power <ec24380@qmul.ac.uk>
2026-03-12 19:05:15 +0000

    Note Spawner now works.

--- Scripts/note.gd
+++ Scripts/note.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-var note_beat = 0
-var speed = 100
-var hit_x = 66
-var lane_y = 324
+var note_beat: float = 0.0
 
-func _ready():
+@export var hit_x: float = 66.0
+@export var spawn_x: float = 1400.0
+@export var lead_beats: float = 8.0
+@export var lane_y: float = 324.0
+
+func _ready() -> void:
 	position.y = lane_y
+	z_index = 100
 
-func _process(delta):
-
-	var beat_diff = note_beat - Conductor.song_position_in_beats
-
-	position.x = hit_x + beat_diff * speed
+func _process(_delta: float) -> void:
+	var beats_until_hit: float = note_beat - float(Conductor.song_position_in_beats)
+	var px_per_beat: float = (spawn_x - hit_x) / lead_beats
+	position.x = hit_x + beats_until_hit * px_per_beat
--- Scripts/note_spawner.gd
+++ Scripts/note_spawner.gd
@@ -1,17 +1,24 @@
 extends Node2D
+
 @onready var notes_container: Node = $"../Notes"
-@export var note_scene : PackedScene
+@export var note_scene: PackedScene
+@export var lead_beats: float = 8.0
 
-var chart = [2, 4, 6, 8]
+var chart: Array[float] = [2.0, 4.0, 6.0, 8.0]
+var next_index: int = 0
 
-func _ready():
+func _process(_delta: float) -> void:
 	if note_scene == null:
-			push_error("NoteSpawner: note_scene is not assigned!")
-			return
-	for beat in chart:
-		spawn_note(beat)
-		
-func spawn_note(beat):
-	var note = note_scene.instantiate()
-	note.note_beat = beat
-	notes_container.add_child(note)
+		push_error("NoteSpawner: note_scene is not assigned!")
+		return
+
+	var current_beat: float = float(Conductor.song_position_in_beats)
+
+	while next_index < chart.size() and (chart[next_index] - current_beat) <= lead_beats:
+		var beat: float = chart[next_index]
+
+		var note: Node = note_scene.instantiate()
+		note.set("note_beat", beat)
+		notes_container.add_child(note)
+
+		next_index += 1
